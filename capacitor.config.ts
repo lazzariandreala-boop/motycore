2ad151a2ce12447a795f0d2328f50ae1e3b30a15
Andrea Lazzari <andrea.lazzari@omnys.com>
2026-03-23 12:16:13 +0100

    added vercel callback

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.motycore.app',
   appName: 'MotyCore',
-  webDir: '.output/public'
+  webDir: '.output/public',
+  server: {
+    url: 'https://motycore.vercel.app/',  // ← il tuo URL Vercel
+    cleartext: false
+  }
 };
 
 export default config;
